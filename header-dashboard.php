4ced643d858c45be17256c61726b20bbc498fbee
attila <attilalopes@hotmail.com>
2014-11-11 02:45:44 -0200

    pagamentos atualizada

--- header-dashboard.php
+++ header-dashboard.php
@@ -33,13 +33,10 @@
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Dashboard</a></li>
-                <li><a href="#">Settings</a></li>
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Help</a></li>
+                <li><a href="#">Configurações</a></li>
+                <li><a href="#">Editar informações</a></li>
+                <li><a href="#">Sair</a></li>
             </ul>
-            <form class="navbar-form navbar-right">
-                <input type="text" class="form-control" placeholder="Search...">
-            </form>
         </div>
     </div>
 </nav>
